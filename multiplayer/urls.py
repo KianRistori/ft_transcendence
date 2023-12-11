67837e48eb45fdbf3408a258cd5ff7b1a13e87ad
jacopo3101 <jacopo.avellis@gmail.com>
2023-12-11 10:20:12 +0100

    1v1 AI

--- multiplayer/urls.py
+++ multiplayer/urls.py
@@ -4,7 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('ai/', views.homeAI, name='home'),
-    path('1vs1/', views.home1vs1, name='home'),
-    path('tournament/', views.homeTournament, name='home'),
 ]
